693ecc643f423d1d144a7b88c13af132925ddc57
brunoar86 <ribeiro256@msn.com>
2023-11-07 22:14:51 -0300

    Incluído 'decorator' no controller para acessar os campos do formulário.

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,8 +1,11 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
         const getter = function () {
             const elemento = document.querySelector(seletor);
+            console.log(`buscando elemento do DOM com o seletor`);
             return elemento;
         };
+        Object.defineProperty(target, propertyKey, { get: getter });
     };
 }
