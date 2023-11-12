52c2d9a9c6c6fe1b69ea3b937bebd0b5b520a263
brunoar86 <ribeiro256@msn.com>
2023-11-12 11:11:28 -0300

    Includes new interface to extend Imprimivel e Comparavel

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -36,6 +36,11 @@ export class NegociacaoController {
     importaDados() {
         this.negociacoesService.obterNegociacoesDoDia()
             .then(negociacoesDeHoje => {
+            return negociacoesDeHoje.filter(negociacaoDeHoje => {
+                return !this.negociacoes.lista().some(negociacao => negociacao.ehIgual(negociacaoDeHoje));
+            });
+        })
+            .then(negociacoesDeHoje => {
             for (let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
             }
