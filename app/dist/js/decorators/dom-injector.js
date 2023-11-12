284d3f9b8ac100644ca4e49416c399a6b268a2b2
brunoar86 <ribeiro256@msn.com>
2023-11-11 22:27:50 -0300

    Includes utils to improve log

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,9 +1,11 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
         console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,8 +1,11 @@
 import { domInjector } from '../decorators/dom-injector.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacoesDoDia } from '../interfaces/negociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
+import { NegociacoesService } from '../services/negociacoes-service.js';
+import { imprimir } from '../utils/imprimir.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -16,6 +19,7 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
+    private negociacoesService = new NegociacoesService();
 
     constructor() {
         this.negociacoesView.update(this.negociacoes);
@@ -38,8 +42,19 @@ export class NegociacaoController {
         }
 
         this.negociacoes.adiciona(negociacao);
+        imprimir(negociacao, this.negociacoes);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    importaDados(): void {
+        this.negociacoesService.obterNegociacoesDoDia()
+            .then(negociacoesDeHoje => {
+                for (let negociacao of negociacoesDeHoje) {
+                    this.negociacoes.adiciona(negociacao);
+                }
+                this.negociacoesView.update(this.negociacoes);
+            });
     }
 
     private ehDiaUtil(data: Date) {
